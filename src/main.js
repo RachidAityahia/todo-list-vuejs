5ee8e44e34b04beacfa8b420d6bdd4b0a4c7a17a
Ait yahia Rachid <aityahyarachid.43@gmmail.com>
2023-03-29 13:37:35 +0000

    projet

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import router from "./router/routes.js" 
+import {store} from './Store' 
+import './style.css'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+const app = createApp(App)
+app.use(store)
+.use(router)
+.use(ElementPlus)
+.mount('#app')
+
+//l'enregistrement global des icones element plus
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
